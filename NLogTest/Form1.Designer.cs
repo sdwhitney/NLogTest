58ff41f338362975f0998d7c3243ad75659188e7
sdw <sdw@inea.com>
2018-10-03 12:54:49 -0400

    Initial commit - demo of NLog with messages to Output (Debugger) window and to dated log file in logs directory under executable.

--- NLogTest/Form1.Designer.cs
+++ NLogTest/Form1.Designer.cs
@@ -28,13 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDebug = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnWarn = new System.Windows.Forms.Button();
+            this.btnError = new System.Windows.Forms.Button();
+            this.btnFatal = new System.Windows.Forms.Button();
+            this.btnTrace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDebug
+            // 
+            this.btnDebug.Location = new System.Drawing.Point(36, 31);
+            this.btnDebug.Name = "btnDebug";
+            this.btnDebug.Size = new System.Drawing.Size(75, 23);
+            this.btnDebug.TabIndex = 0;
+            this.btnDebug.Text = "Debug";
+            this.btnDebug.UseVisualStyleBackColor = true;
+            this.btnDebug.Click += new System.EventHandler(this.btnDebug_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(36, 60);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(75, 23);
+            this.btnInfo.TabIndex = 1;
+            this.btnInfo.Text = "Info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnWarn
+            // 
+            this.btnWarn.Location = new System.Drawing.Point(36, 89);
+            this.btnWarn.Name = "btnWarn";
+            this.btnWarn.Size = new System.Drawing.Size(75, 23);
+            this.btnWarn.TabIndex = 2;
+            this.btnWarn.Text = "Warn";
+            this.btnWarn.UseVisualStyleBackColor = true;
+            this.btnWarn.Click += new System.EventHandler(this.btnWarn_Click);
+            // 
+            // btnError
+            // 
+            this.btnError.Location = new System.Drawing.Point(36, 118);
+            this.btnError.Name = "btnError";
+            this.btnError.Size = new System.Drawing.Size(75, 23);
+            this.btnError.TabIndex = 3;
+            this.btnError.Text = "Error";
+            this.btnError.UseVisualStyleBackColor = true;
+            this.btnError.Click += new System.EventHandler(this.btnError_Click);
+            // 
+            // btnFatal
+            // 
+            this.btnFatal.Location = new System.Drawing.Point(36, 147);
+            this.btnFatal.Name = "btnFatal";
+            this.btnFatal.Size = new System.Drawing.Size(75, 23);
+            this.btnFatal.TabIndex = 4;
+            this.btnFatal.Text = "Fatal";
+            this.btnFatal.UseVisualStyleBackColor = true;
+            this.btnFatal.Click += new System.EventHandler(this.btnFatal_Click);
+            // 
+            // btnTrace
+            // 
+            this.btnTrace.Location = new System.Drawing.Point(36, 176);
+            this.btnTrace.Name = "btnTrace";
+            this.btnTrace.Size = new System.Drawing.Size(75, 23);
+            this.btnTrace.TabIndex = 5;
+            this.btnTrace.Text = "Trace";
+            this.btnTrace.UseVisualStyleBackColor = true;
+            this.btnTrace.Click += new System.EventHandler(this.btnTrace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTrace);
+            this.Controls.Add(this.btnFatal);
+            this.Controls.Add(this.btnError);
+            this.Controls.Add(this.btnWarn);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnDebug);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDebug;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnWarn;
+        private System.Windows.Forms.Button btnError;
+        private System.Windows.Forms.Button btnFatal;
+        private System.Windows.Forms.Button btnTrace;
     }
 }
 
--- NLogTest/Form1.cs
+++ NLogTest/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,41 @@ namespace NLogTest
 {
     public partial class Form1 : Form
     {
+        public static Logger LogInstance { get; private set; }
         public Form1()
         {
             InitializeComponent();
+            LogInstance = LogManager.GetCurrentClassLogger();
+        }
+
+        private void btnDebug_Click(object sender, EventArgs e)
+        {
+            LogInstance.Debug("Debug message");
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            LogInstance.Info("Info message");
+        }
+
+        private void btnWarn_Click(object sender, EventArgs e)
+        {
+            LogInstance.Warn("Warn message");
+        }
+
+        private void btnError_Click(object sender, EventArgs e)
+        {
+            LogInstance.Error("Error message");
+        }
+
+        private void btnFatal_Click(object sender, EventArgs e)
+        {
+            LogInstance.Fatal("Fatal message");
+        }
+
+        private void btnTrace_Click(object sender, EventArgs e)
+        {
+            LogInstance.Trace("Trace message");
         }
     }
 }
